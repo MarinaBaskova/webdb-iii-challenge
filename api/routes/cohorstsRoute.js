d7658150000380a50456a495cd1d6331f3e6495b
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-01 14:44:30 -0700

    added migrations for cohorts, students, added routes for cohorts

---
+++ api/routes/cohorstsRoute.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../config/dbConfig');
+
+router.get('/', (req, res) => {
+	db('cohorts')
+		.then((cohorts) => {
+			res.status(200).json(cohorts);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'The cohorts information could not be retrieved.' });
+		});
+});
+
+router.get('/:id', (req, res) => {
+	db('cohorts')
+		.where({ id: req.params.id })
+		.first()
+		.then((cohort) => {
+			if (cohort) {
+				res.status(200).json(cohort);
+			} else {
+				res.status(404).json({ error: 'The cohort with the specified ID does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'The cohort information could not be retrieved.' });
+		});
+});
+
+router.post('/', (req, res) => {
+	if (!req.body.name) {
+		res.status(400).json({ error: 'Please provide a name for a new cohort' });
+	} else {
+		db('cohorts')
+			.insert(req.body, 'id')
+			.then((idOfNewCohort) => {
+				res.status(201).json(idOfNewCohort);
+			})
+			.catch((err) => {
+				res.status(500).json({ error: 'There was an error while saving the cohort to the database' });
+			});
+	}
+});
+
+router.put('/:id', (req, res) => {
+	db('cohorts')
+		.where({ id: req.params.id })
+		.update(req.body)
+		.then((numOUpdated) => {
+			if (numOUpdated > 0) {
+				db('cohorts')
+					.where({ id: req.params.id })
+					.first()
+					.then((cohort) => {
+						res.status(200).json(cohort);
+					})
+					.catch((err) => {
+						res.status(404).json({ error: 'The cohort with the specified ID does not exist.' });
+					});
+			} else {
+				res.status(404).json({ message: 'It was an error while updating your cohort, please try again' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
+
+router.delete('/:id', (req, res) => {
+	db('cohorts')
+		.where({ id: req.params.id })
+		.del()
+		.then((numOfDeleted) => {
+			if (numOfDeleted > 0) {
+				res.status(204).end();
+			} else {
+				res.status(404).json({ error: 'The cohort with the specified ID does not exist.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'It was an error while deleting your cohort, please try again' });
+		});
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const cohortsRouter = require('../api/routes/cohorstsRoute');
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use('/api/cohorts', cohortsRouter);
 
 module.exports = server;
